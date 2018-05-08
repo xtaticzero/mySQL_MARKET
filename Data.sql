feee24f358a9509444633691caf2640e2a37537a
xtaticzero <>
2018-05-08 17:33:00 -0500

    carga de datos

--- Data.sql
+++ Data.sql
@@ -1,3 +1,10 @@
 --Data Application
 USE STOCK_MARKET_BBDD;
 INSERT ROL(descripcion) VALUES('ADMINISTRADOR');
+INSERT ROL(descripcion) VALUES('OPERADOR');
+
+INSERT INTO USERS (email, display_name, password, rol_id,fecha)
+VALUES ('user@dominio', 'admin', MD5('admin123'), 1,SYSDATE());
+
+INSERT INTO USERS (email, display_name, password, rol_id,fecha)
+VALUES ('operador@dominio', 'operador', MD5('operador123'), 1,SYSDATE());
