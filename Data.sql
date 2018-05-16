4363873566058fb4e3223c0d709299d5250aaa57
Emmanuel <eestradag0300@gmail.com>
2018-05-15 19:52:32 -0500

    insert de datos

--- Data.sql
+++ Data.sql
@@ -11,3 +11,16 @@ INSERT INTO USERS (email, display_name, password, rol_id,fecha)
 VALUES ('operador@dominio.com', 'operador', MD5('operador123'), 1,SYSDATE());
 --COMISIONES DEL APLICATIVO
 INSERT INTO COMISIONES(PORCENTAJE,DESCRIPCION) VALUES(0.00174,"Comisiones Casas de bolsa");
+
+--EMISORAS
+INSERT EMISORA(nombre,fechaEntrada) VALUES('ALFA',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('AMX',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('BIMBO',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('CEMEX',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('FEMSA',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('GENTERA',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('GFNORTE',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('GMEXICO',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('KIMBER',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('NAFTRAC',SYSDATE());
+INSERT EMISORA(nombre,fechaEntrada) VALUES('WALMEX',SYSDATE());
